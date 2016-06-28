f812f545c3902478037804abf08aa64323a3dbae
Mark Alston <malston@pivotal.io>
2016-06-28 07:47:15 -0600

    Update ansible to 1.9.3

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Installing Ansible..."
-#apt-get update -y
-apt-get install -y python-pip python-dev
-pip install ansible==1.9.2
+apt-get update -y
+apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip
+pip install ansible==1.9.3
 mkdir -p /etc/ansible
 touch /etc/ansible/hosts
