94c430e308607ef0d3d2f8894b4eb5b2025c56a6
Mark Alston <malston@pivotal.io>
2016-06-28 07:46:50 -0600

    Update ansible to 1.9.3

--- scripts/preload_cje.sh
+++ scripts/preload_cje.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-ansible-playbook /vagrant/ansible/cje.yml -c local --extra-vars 'skip_licence=true'
+#ansible-playbook /vagrant/ansible/cje.yml -c local --extra-vars 'skip_licence=true'
+ansible-playbook /vagrant/ansible/cje.yml -c local --extra-vars 'preload=true'
 
 ansible-playbook /vagrant/ansible/node.yml -c local
