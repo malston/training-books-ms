e0ffe155b817ddcc9f0e322430940fb1434b29f3
Mark Alston <malston@pivotal.io>
2016-07-01 17:10:27 -0600

    Add script to install java 8

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -4,7 +4,15 @@ set -e
 
 echo "Installing Ansible..."
 apt-get update -y
-apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip
+apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip maven
 pip install ansible==1.9.3
 mkdir -p /etc/ansible
 touch /etc/ansible/hosts
+
+# Uncomment to run java on slave without docker
+# echo "Installing openjdk-8-jdk..."
+# sudo add-apt-repository -y ppa:openjdk-r/ppa
+# sudo apt-get update -y
+# sudo apt-get install -y openjdk-8-jdk
+# sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+# sudo update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
