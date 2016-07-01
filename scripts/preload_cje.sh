a84d333ab8dd7f6fa47c9e700994876d571fb086
Mark Alston <malston@pivotal.io>
2016-06-30 21:29:15 -0600

    Update pipeline configs

--- scripts/preload_cje.sh
+++ scripts/preload_cje.sh
@@ -3,6 +3,6 @@
 set -e
 
 #ansible-playbook /vagrant/ansible/cje.yml -c local --extra-vars 'skip_licence=true'
-ansible-playbook /vagrant/ansible/cje.yml -c local --extra-vars 'preload=true'
+ansible-playbook /vagrant/ansible/cje.yml -c local --extra-vars 'skip_licence=true preload=true'
 
 ansible-playbook /vagrant/ansible/node.yml -c local
